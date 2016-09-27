37ccdddba50fd80bfa9218ad1dbd347a4d209e35
ariverr <ariverr@gmail.com>
2016-09-27 19:47:19 +0500

    Add files via upload

---
+++ ArduinoUno_I2C_Master.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+  for(int x = 1; x < 4; x++)
+  {
+    Wire.requestFrom(x,1); //request from slave device
+                           //address 1. expect 1 byte
+    while(Wire.available())
+    {
+      int i = Wire.read();
+      
+      String rString = "";
+      rString.concat(x);
+      rString.concat(" value is ");
+      rString.concat(i);
+      Serial.println(rString);
+    }
+    delay(500);
+  }
+}
+  
